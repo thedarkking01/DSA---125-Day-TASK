ab1f88d1ea1b10005e6d0732874999a2f382d78d
sourabh singh bais <sourabhsinghbais52@gmail.com>
2024-10-02 00:15:22 +0530

    Day 38

--- Linked list/Medium/Swap Nodes in Pairs.py
+++ Linked list/Medium/Swap Nodes in Pairs.py
@@ -5,8 +5,26 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        cur=head
+        # cur=head
+        # while cur and cur.next:
+        #     cur.val,cur.next.val=cur.next.val,cur.val
+        #     cur=cur.next.next
+        # return head
+
+        dummy = ListNode(0, head)
+        prev, cur = dummy, head
         while cur and cur.next:
-            cur.val,cur.next.val=cur.next.val,cur.val
-            cur=cur.next.next
-        return head
+            # saved pointers
+            nxtpt=cur.next.next
+            second=cur.next
+            
+            #reversed
+            second.next=cur
+            cur.next=nxtpt
+            prev.next=second
+
+            #update ptr
+            prev=cur
+            cur=nxtpt
+
+        return dummy.next
