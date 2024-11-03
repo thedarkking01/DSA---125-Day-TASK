dd2899133dcc9910c0ef79dfd7b0f2381e1a842a
sourabh singh bais <sourabhsinghbais52@gmail.com>
2024-11-03 14:51:46 +0530

    Day 61

--- Graph/Number of Islands.py
+++ Graph/Number of Islands.py
@@ -26,3 +26,37 @@ class Solution:
                     island+=1
                     dfs(r,c)
         return island
+    
+    # for BFS way
+
+    class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        rows,cols=len(grid),len(grid[0])
+        island=0
+        visit=set()
+
+        def bfs(r,c):
+            q=collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+            
+            while q:
+
+                row,col=q.popleft()  # if we only pop() then it will be DFS 
+                directions=[[1,0],[-1,0],[0,1],[0,-1]]
+                for dr,dc in directions: 
+                    r,c=row+dr,col+dc
+                    if (r in range(rows) and
+                        c in range(cols) and
+                        grid[r][c]=='1' and (r,c) not in visit ):
+                        q.append((r,c))
+                        visit.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1" and (r,c) not in visit:
+                    island+=1
+                    bfs(r,c)
+        return island
