013be7bfade8adf302e1c9c235e948bbd6b0ec2a
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-01-17 20:34:41 +0530

    day 85

---
+++ Array and hashing/Minimum Right Shifts to Sort the Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumRightShifts(self, nums: List[int]) -> int:
+        n=len(nums)
+        for i in range(1,n):
+            if nums[i] < nums[i-1]:
+                nums = nums[i:]+nums[:i]
+                return n-i if nums == sorted(nums) else -1
+        return 0
