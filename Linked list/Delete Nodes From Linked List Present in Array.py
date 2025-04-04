a1604629dd327cdb23d4fae8e36f3ce820a30168
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-04-04 14:15:03 +0530

    Day 113

--- Linked list/Delete Nodes From Linked List Present in Array.py
+++ Linked list/Delete Nodes From Linked List Present in Array.py
@@ -5,14 +5,29 @@
 #         self.next = next
 
 # by single pointer
+# class Solution:
+#     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+#         dummy=ListNode(0,head)
+#         nums=set(nums)
+#         prev=dummy
+#         while prev.next:
+#             if prev.next.val in nums:
+#                 prev.next=prev.next.next
+#             else:
+#                 prev=prev.next
+#         return dummy.next
+
+# by two pointers
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
         dummy=ListNode(0,head)
         nums=set(nums)
         prev=dummy
-        while prev.next:
-            if prev.next.val in nums:
-                prev.next=prev.next.next
+        cur=head
+        while cur:
+            if cur.val in nums:
+                prev.next=cur.next
             else:
-                prev=prev.next
+                prev=cur
+            cur=cur.next
         return dummy.next
