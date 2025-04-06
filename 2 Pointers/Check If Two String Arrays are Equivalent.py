fe12436d98bfb5b5466e594ff3448385685d2e50
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-04-06 22:00:52 +0530

    Day 114

---
+++ 2 Pointers/Check If Two String Arrays are Equivalent.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        w1=w2=""
+        for i in word1:
+            w1+=i
+        for i in word2:
+            w2+=i
+        return w1==w2
