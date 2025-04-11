d508b531931298f47618c24b21811edb486a2fa6
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-04-11 13:23:53 +0530

    Day 117

--- Linked list/Remove Nodes From Linked List.py
+++ Linked list/Remove Nodes From Linked List.py
@@ -1,3 +1,30 @@
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         stack = []
+#         cur = head
+        
+#         # Traverse the list and maintain a decreasing stack
+#         while cur:
+#             while stack and cur.val > stack[-1]:
+#                 stack.pop()
+#             stack.append(cur.val)
+#             cur = cur.next 
+
+#         # Rebuild the list from the filtered values
+#         dummy = ListNode()
+#         cur = dummy
+#         for val in stack:
+#             cur.next = ListNode(val)
+#             cur = cur.next
+#         return dummy.next
+
+
+#Reverse twice
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,20 +32,23 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        stack = []
-        cur = head
-        
-        # Traverse the list and maintain a decreasing stack
-        while cur:
-            while stack and cur.val > stack[-1]:
-                stack.pop()
-            stack.append(cur.val)
-            cur = cur.next 
+        def reverse(head):
+            prev, cur = None, head
+            while cur:
+                tmp = cur.next
+                cur.next = prev
+                prev, cur = cur, tmp
+            return prev
 
-        # Rebuild the list from the filtered values
-        dummy = ListNode()
-        cur = dummy
-        for val in stack:
-            cur.next = ListNode(val)
-            cur = cur.next
-        return dummy.next
+        head = reverse(head)
+        cur = head
+        cur_max = head.val
+
+        while cur and cur.next:
+            if cur.next.val < cur_max:
+                cur.next = cur.next.next
+            else:
+                cur_max = cur.next.val
+                cur = cur.next
+                
+        return reverse(head)
