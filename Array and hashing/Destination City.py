895d0eda1d1262439e4f476c4bfb1ad22c48b609
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-04-21 13:24:42 +0530

    Day 127

---
+++ Array and hashing/Destination City.py
@@ -0,0 +1,8 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        s=set()
+        for p in paths:
+            s.add(p[0])
+        for p in paths:
+            if p[1] not in s:
+                return p[1]
