631c0e969a5fe140bb89117bdad93708667c999e
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-05-08 20:40:12 +0530

    Day 138

---
+++ Stack/Clear Digits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        res=[]
+        delete_cnt=0
+        def isdigit(c):
+            return ord('0')<=ord(c)<=ord('9')
+        for i in range(len(s)-1,-1,-1):
+            if isdigit(s[i]):
+                delete_cnt+=1
+            elif delete_cnt:
+                delete_cnt-=1
+            else:
+                res.append(s[i])
+        return "".join(res[::-1])
