f7d09a865fe802755bf85d802fc341810dbdb6d6
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-05-11 19:57:48 +0530

    Day 140

---
+++ Stack/Reverse Substrings Between Each Pair of Parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack=[]
+        for c in s:
+            if c==")":
+                portion=[]
+                while stack[-1]!="(":
+                    portion.append(stack.pop())
+                stack.pop()
+                stack.extend(portion)
+            else:
+                stack.append(c)
+        return "".join(stack)
