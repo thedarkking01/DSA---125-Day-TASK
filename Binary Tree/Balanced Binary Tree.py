208a33e30063ff80e05fb4c7d5660dcf7223a380
SOURABH SINGH BAIS <110977868+thedarkking01@users.noreply.github.com>
2025-05-23 23:06:09 +0530

    Update Balanced Binary Tree.py

--- Binary Tree/Balanced Binary Tree.py
+++ Binary Tree/Balanced Binary Tree.py
@@ -13,3 +13,29 @@ class Solution:
             balanced=left[0] and right[0] and abs(left[1] - right[1]) <= 1
             return [balanced,1+max(left[1],right[1])]
         return dfs(root)[0]
+
+
+# Using bottom-up recursion
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.balanced = True
+
+        def height(node):
+            if not node:
+                return 0
+            left_height = height(node.left)
+            right_height = height(node.right)
+            if abs(left_height - right_height) > 1:
+                self.balanced = False
+            return 1 +max(left_height,right_height)
+
+        height(root)
+        return self.balanced
