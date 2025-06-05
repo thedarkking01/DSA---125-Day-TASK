75f3e6ea455f967e55951141133880707e515fc6
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-06-06 00:06:00 +0530

    Day 156

--- Array and hashing/Pascal's Triangle.py
+++ Array and hashing/Pascal's Triangle.py
@@ -1,10 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res=[[1]]
+        result=[[1]]
         for i in range(numRows - 1):
-            temp = [0]+res[-1]+[0]
+            temp = [0]+result[-1]+[0]
             row=[]
-            for j in range(len(res[-1]) + 1):
+            for j in range(len(result[-1]) + 1):
                 row.append(temp[j]+temp[j+1])
-            res.append(row)
-        return res
+            result.append(row)
+        return result
