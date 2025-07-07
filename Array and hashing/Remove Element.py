16d3c3a925d9b55b62b0d07de70809792da5de77
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-07-07 23:32:41 +0530

    Day 167

--- Array and hashing/Remove Element.py
+++ Array and hashing/Remove Element.py
@@ -10,3 +10,15 @@ class Solution:
         return k
     
 
+# 2 pointers
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i=0
+        n=len(nums)
+        while i<n:
+            if nums[i]!=val:
+                nums[i]=nums[n]
+                n-=1
+            else:
+                i+=1
+        return n
