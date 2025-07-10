a51059c81a810af58c0543599674b31433fd7906
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-07-10 23:14:22 +0530

    Day 169

---
+++ Array and hashing/Longest Palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        seen = set()
+        res = 0
+        for c in s:
+            if c in seen:
+                seen.remove(c)
+                res += 2
+            else:
+                seen.add(c)
+        return res + 1 if seen else res
