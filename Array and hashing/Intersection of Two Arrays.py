aee0b1d001b2f799a7b28baed0df88c2c68e4656
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-07-24 22:41:15 +0530

    Day 175

--- Array and hashing/Intersection of Two Arrays.py
+++ Array and hashing/Intersection of Two Arrays.py
@@ -7,3 +7,12 @@ class Solution:
             if num in set2:
                 res.append(num)
         return res
+
+
+# Time: O(n)
+# Space: O(n)
+# Built-In Functions
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+            return list(set(nums1) & set(nums2))
