29c1628fd0c1e671411fd0f4f04338388d52283d
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-07-25 23:27:39 +0530

    Day 176

--- Array and hashing/Circular Sentence.py
+++ Array and hashing/Circular Sentence.py
@@ -5,3 +5,12 @@ class Solution:
             if w[i][0]!=w[i-1][-1]:
                 return False
         return True
+    
+
+# iterative solution(space optimized)
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        for i in range(len(sentence)):
+            if sentence[i] == " " and sentence[i - 1] != sentence[i + 1]:
+                return False
+        return sentence[0] == sentence[-1]
