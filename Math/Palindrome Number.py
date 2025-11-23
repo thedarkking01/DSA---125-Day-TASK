3371e3e98ff0db67f80d0cdff2864c6959656043
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-11-23 23:07:39 +0530

    day 199

---
+++ Math/Palindrome Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False 
+        rev=0
+        orig=x
+        while x!=0:
+            rem=x%10
+            rev=rev*10+rem
+            x=x//10
+        return rev==orig
