35997e84839f1d6286cb89061a973da1986b261e
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-12-05 23:33:39 +0530

    Day 209

---
+++ Math/Prime Arrangements.py
@@ -0,0 +1,22 @@
+class Solution:
+    mod = 10**9 + 7
+    
+    def numPrimeArrangements(self, n: int) -> int:
+        def factorial(num):
+            res = 1
+            for i in range(2, num + 1):
+                res = (res * i) % self.mod
+            return res
+        
+        def is_prime(num):
+            if num <= 1:
+                return False
+            for i in range(2, int(num**0.5) + 1):
+                if num % i == 0:
+                    return False
+            return True
+        
+        total_prime = sum(1 for i in range(1, n + 1) if is_prime(i))
+        non_prime = n - total_prime
+        fact = (factorial(total_prime) * factorial(non_prime)) % self.mod
+        return fact
