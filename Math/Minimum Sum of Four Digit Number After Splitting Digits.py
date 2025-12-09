a3045cb0c6f28ac2b140343a9a05c14f93f04c19
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-12-09 23:42:51 +0530

    improve code

--- Math/Minimum Sum of Four Digit Number After Splitting Digits.py
+++ Math/Minimum Sum of Four Digit Number After Splitting Digits.py
@@ -3,11 +3,11 @@
 
 class Solution:
     def minimumSum(self, num: int) -> int:
-        res = [0] * 4
+        result = [0] * 4
         i = 0
         while num > 0:
-            res[i] = num % 10
+            result[i] = num % 10
             num //= 10
             i += 1
-        res.sort()
-        return res[0] * 10 + res[2] + res[1] * 10 + res[3]
+        result.sort()
+        return result[0] * 10 + result[2] + result[1] * 10 + result[3]
