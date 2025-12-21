6b04e700c114bb7ce76b4deffb4b2e9c4a1df384
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-12-21 18:20:19 +0530

    add problem statement

--- Math/Palindrome Number.py
+++ Math/Palindrome Number.py
@@ -1,3 +1,7 @@
+# Problem Statement :
+
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x<0:
