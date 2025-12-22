0e6cac0ac53f58b836e7e9147546aaa5afe88131
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-12-22 22:31:54 +0530

    add problem statement

--- Math/Palindrome Number.py
+++ Math/Palindrome Number.py
@@ -2,6 +2,22 @@
 
 # Given an integer x, return true if x is a palindrome, and false otherwise.
 
+# Examples:
+
+# Example 1:
+
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+ 
+
+# Example 2:
+
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x<0:
